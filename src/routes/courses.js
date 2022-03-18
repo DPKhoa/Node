4710d5d0b6d928eea0e329717ab28e280c3c80d6
DPKhoa <anhk312001@gmail.com>
2022-03-18 08:32:35 +0700

    create new courses

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 const courseController =  require('../app/controllers/CourseController');
 
+router.get('/create', courseController.create);
+router.post('/store', courseController.store);
 router.get('/:slug', courseController.show);
-
 //newsConstroller.index
 module.exports = router;
