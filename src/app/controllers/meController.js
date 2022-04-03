b4d816ddace638435f0615483866142585ae03b6
DPKhoa <anhk312001@gmail.com>
2022-04-03 14:32:38 +0700

    soft delete

--- src/app/controllers/meController.js
+++ src/app/controllers/meController.js
@@ -3,13 +3,27 @@ const {mutipleMongooseToObject} = require('../../util/mongoose')
 class meController {
     //[GET]/me/stored/courses
     storedCreate(req, res, next) {
-        Course.find({})
-        .then(courses => res.render('me/stored-courses', {
-            courses:mutipleMongooseToObject(courses)
-        }))
+        Promise.all([Course.find({}), Course.countDocumentsDeleted()])
+        .then(([courses, deleteCount])=>
+            res.render('me/stored-courses', {
+                deleteCount,
+                courses:mutipleMongooseToObject(courses)
+            })
+        )
         .catch(next);
+
+    //     Course.countDocumentsDeleted()
+    //     .then((deleteCount) => {
+    //         console.log(deleteCount);
+    //     })
+    //     .catch(()=>{})
+    //     Course.find({})
+    //     .then(courses => res.render('me/stored-courses', {
+    //         courses:mutipleMongooseToObject(courses)
+    //     }))
+    //     .catch(next);
         
-    }
+ }
 
     trashCourses(req, res, next){
         Course.findDeleted({})
